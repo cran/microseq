b3e83b5424bdd6fe2a36d29ab36f26c091376397
Lars Snipen <lars.snipen@nmbu.no>
2023-08-21 11:40:02 +0000

    version 2.1.6

--- R/orfs.R
+++ R/orfs.R
@@ -4,40 +4,53 @@
 #' @description Finds all ORFs in prokaryotic genome sequences.
 #' 
 #' @param genome A fasta object (\code{tibble}) with the genome sequence(s).
-#' @param circular Logical indicating if the genome sequences are completed, circular sequences.
+#' @param circular Logical indicating if the genome sequences are completed, 
+#' circular sequences.
 #' @param trans.tab Translation table.
 #' 
-#' @details A prokaryotic Open Reading Frame (ORF) is defined as a subsequence starting with a  start-codon
-#' (ATG, GTG or TTG), followed by an integer number of triplets (codons), and ending with a stop-codon (TAA,
-#' TGA or TAG, unless \code{trans.tab = 4}, see below). This function will locate all such ORFs in a genome.
+#' @details A prokaryotic Open Reading Frame (ORF) is defined as a sub-sequence 
+#' starting with a  start-codon (ATG, GTG or TTG), followed by an integer number
+#' of triplets (codons), and ending with a stop-codon (TAA, TGA or TAG, unless 
+#' \code{trans.tab = 4}, see below). This function will locate all such ORFs in
+#' a genome.
 #' 
-#' The argument \code{genome} is a fasta object, i.e. a table with columns \samp{Header} and \samp{Sequence},
-#' and will typically have several sequences (chromosomes/plasmids/scaffolds/contigs).
-#' It is vital that the \emph{first token} (characters before first space) of every \samp{Header} is
-#' unique, since this will be used to identify these genome sequences in the output.
+#' The argument \code{genome} is a fasta object, i.e. a table with columns 
+#' \samp{Header} and \samp{Sequence}, and will typically have several sequences
+#' (chromosomes/plasmids/scaffolds/contigs). It is vital that the \emph{first 
+#' token} (characters before first space) of every \samp{Header} is
+#' unique, since this will be used to identify these genome sequences in the
+#' output.
 #' 
-#' An alternative translation table may be specified, and as of now the only alternative implemented is table 4.
-#' This means codon TGA is no longer a stop, but codes for Tryptophan. This coding is used by some bacteria
-#' (e.g. Mycoplasma, Mesoplasma).
+#' By default the genome sequences are assumed to be linear, i.e. contigs or
+#' other incomplete fragments of a genome. In such cases there will usually be
+#' some truncated ORFs at each end, i.e. ORFs where either the start- or the 
+#' stop-codon is lacking. In the \code{orf.table} returned by this function this
+#' is marked in the \samp{Attributes} column. The texts "Truncated=10" or 
+#' "Truncated=01" indicates truncated at the beginning or end of the genomic 
+#' sequence, respectively. If the supplied \code{genome} is a completed genome,
+#' with circular chromosome/plasmids, set the flag \code{circular = TRUE} and no
+#' truncated ORFs will be listed. In cases where an ORF runs across the origin 
+#' of a circular genome sequences, the stop coordinate will be larger than the 
+#' length of the genome sequence. This is in line with the specifications of
+#' the GFF3 format, where a \samp{Start} cannot be larger than the
+#' corresponding \samp{End}.
 #' 
-#' Note that for any given stop-codon there are usually multiple start-codons in the same reading
-#' frame. This function will return all, i.e. the same stop position may appear multiple times. If
-#' you want ORFs with the most upstream start-codon only (LORFs), then filter the output from this function
-#' with \code{\link{lorfs}}.
+#' An alternative translation table may be specified, and as of now the only
+#' alternative implemented is table 4. This means codon TGA is no longer a stop,
+#' but codes for Tryptophan. This coding is used by some bacteria
+#' (e.g. under the orders Entomoplasmatales and Mycoplasmatales).
 #' 
-#' By default the genome sequences are assumed to be linear, i.e. contigs or other incomplete fragments
-#' of a genome. In such cases there will usually be some truncated ORFs at each end, i.e. ORFs where either
-#' the start- or the stop-codon is lacking. In the \code{orf.table} returned by this function this is marked in the
-#' \samp{Attributes} column. The texts "Truncated=10" or "Truncated=01" indicates truncated at 
-#' the beginning or end of the genomic sequence, respectively. If the supplied \code{genome} is a completed genome, with 
-#' circular chromosome/plasmids, set the flag \code{circular = TRUE} and no truncated ORFs will be listed.
-#' In cases where an ORF runs across the origin of a circular genome sequences, the stop coordinate will be
-#' larger than the length of the genome sequence. This is in line with the specifications of the GFF3 format, where 
-#' a \samp{Start} cannot be larger than the corresponding \samp{End}.
+#' Note that for any given stop-codon there are usually multiple start-codons 
+#' in the same reading frame. This function will return all such nested ORFs, 
+#' i.e. the same stop position may appear multiple times. If you want ORFs with
+#' the most upstream start-codon only (LORFs), then filter the output from this
+#' function with \code{\link{lorfs}}.
 #' 
-#' @return This function returns an \code{orf.table}, which is simply a \code{\link{tibble}} with columns
-#' adhering to the GFF3 format specifications (a \code{gff.table}), see \code{\link{readGFF}}. If you want to retrieve
-#' the ORF sequences, use \code{\link{gff2fasta}}.
+#' 
+#' @return This function returns an \code{orf.table}, which is simply a 
+#' \code{\link{tibble}} with columns adhering to the GFF3 format specifications
+#' (a \code{gff.table}), see \code{\link{readGFF}}. If you want to retrieve
+#' the actual ORF sequences, use \code{\link{gff2fasta}}.
 #' 
 #' 
 #' @author Lars Snipen and Kristian Hovde Liland.
@@ -89,7 +102,7 @@ findOrfs <- function(genome, circular = F, trans.tab = 11){
     mutate(Attributes = "Truncated=00") %>% 
     mutate(Attributes = if_else(.data$Truncated > 0, "Truncated=10", .data$Attributes)) %>% 
     mutate(Attributes = if_else(.data$Truncated < 0, "Truncated=01", .data$Attributes)) %>% 
-    mutate(Source = "micropan::findOrfs") %>% 
+    mutate(Source = "microseq::findOrfs") %>% 
     mutate(Type = "ORF", Score = NA, Phase = 0) %>% 
     select(.data$Seqid, .data$Source, .data$Type, .data$Start, .data$End,
            .data$Score, .data$Strand, .data$Phase, .data$Attributes) -> orf.table
@@ -129,14 +142,15 @@ circularize <- function(ot, trans.tab){
 #' 
 #' @description Computing the lengths of all ORFs in an \code{orf.table}.
 #' 
-#' @param orf.tbl A \code{tibble} with the nine columns of the GFF-format (see \code{\link{findOrfs}}).
+#' @param orf.table A GFF-formatted \code{tibble}.
 #' @param aa Logical, length in amino acids instead of bases.
 #' 
-#' @details Computes the length of an ORF in bases, including the stop codon. However, if
-#' \code{aa = TRUE}, then the length is in amino acids after translation. This aa-length is the
-#' base-length divided by 3 and minus 1, unless the ORF is truncated and lacks a stop codon.
+#' @details By default, computes the length of an ORF in bases, including the
+#' stop codon. However, if \code{aa = TRUE}, then the length is in amino acids
+#' after translation. This aa-length is the base-length divided by 3 and minus 
+#' 1, unless the ORF is truncated and lacks a stop codon.
 #' 
-#' @return A vector of integers.
+#' @return A vector of lengths.
 #' 
 #' @author Lars Snipen and Kristian Hovde Liland.
 #' 
@@ -144,24 +158,21 @@ circularize <- function(ot, trans.tab){
 #' 
 #' @examples # See the example in the Help-file for findOrfs.
 #' 
-#' @importFrom dplyr mutate if_else %>%
+#' @importFrom dplyr if_else
 #' @importFrom stringr str_detect
-#' @importFrom rlang .data
 #' 
 #' @export orfLength
 #' 
-orfLength <- function(orf.tbl, aa = FALSE){
-  orf.tbl %>% 
-    mutate(Length = abs(.data$Start - .data$End) + 1) -> orf.tbl
+orfLength <- function(orf.table, aa = FALSE){
+  length <- abs(orf.table$Start - orf.table$End) + 1
   if(aa){
-    orf.tbl %>% 
-      mutate(Length = .data$Length / 3) %>% 
-      mutate(Length = if_else((str_detect(.data$Attributes, "Truncated=01") & .data$Strand == "+")
-                              |(str_detect(.data$Attributes, "Truncated=10") & .data$Strand == "-"),
-                              .data$Length, .data$Length - 1)) -> orf.tbl
+    length <- if_else((str_detect(orf.table$Attributes, "Truncated=01") & orf.table$Strand == "+")
+                      |(str_detect(orf.table$Attributes, "Truncated=10") & orf.table$Strand == "-"),
+                      length/ 3, length / 3 - 1)
   }
-  return(orf.tbl$Length)
+  return(length)
 }
+
 
 
 #' @name lorfs
@@ -185,7 +196,7 @@ orfLength <- function(orf.tbl, aa = FALSE){
 #' 
 #' @examples # See the example in the Help-file for findOrfs.
 #' 
-#' @importFrom dplyr mutate arrange distinct select desc %>% 
+#' @importFrom dplyr mutate group_by slice_max ungroup select %>% 
 #' @importFrom rlang .data
 #' 
 #' @export lorfs
@@ -193,11 +204,83 @@ orfLength <- function(orf.tbl, aa = FALSE){
 lorfs <- function(orf.tbl){
   Length <- orfLength(orf.tbl)
   orf.tbl %>% 
-    mutate(Length = Length) %>% 
-    arrange(desc(.data$Length)) %>% 
-    mutate(End.end = if_else(.data$Strand == "+", .data$End, .data$Start)) %>% 
-    mutate(Signature = str_c(.data$Seqid, .data$End.end, .data$Strand)) %>% 
-    distinct(.data$Signature, .keep_all = T) %>% 
+    mutate(Signature = orfSignature(.data, full = F)) %>% 
+    mutate(Length = orfLength(.data)) %>% 
+    group_by(.data$Signature) %>% 
+    slice_max(Length) %>% 
+    ungroup() %>% 
     mutate(Type = "LORF") %>% 
+    select(-.data$Length, -.data$Signature) %>% 
     return()
+}
+
+
+
+
+#' @name orfSignature
+#' @title Signature for each ORF
+#' 
+#' @description Creates a signature text for orfs in an \code{orf.table}.
+#' 
+#' @param orf.table A \code{tibble} with ORF information.
+#' @param full Logical indicating type of signature.
+#' 
+#' @details A signature is a text that uniquely identifies each ORF in an
+#' \code{orf.table}, which is a GFF-table with columns \code{Seqid}, \code{Start},
+#' \code{End} and \code{Strand}.
+#' 
+#' The full signature (\code{full = TRUE}) contains the \code{Seqid}, \code{Start},
+#' \code{End} and \code{Strand} information for each ORF, separated by 
+#' semicolon \code{";"}. This text is always unique 
+#' to each ORF. If \code{full = FALSE} the \code{Signature} will not contain 
+#' the starting position information for each ORF. This means all nested ORFs ending 
+#' at the same stop-codon will then get identical \code{Signature}s. This is 
+#' useful for identifying which ORFs are nested within the same LORF.
+#' 
+#' Note that the signature you get with \code{full = FALSE} contains \code{Seqid}, 
+#' then \code{End} if on the positive \code{Strand}, \code{Start} otherwise, and then
+#' the \code{Strand}.
+#' 
+#' @return A text vector with the \code{Signature} for each ORF.
+#' 
+#' @author Lars Snipen.
+#' 
+#' @seealso \code{\link{findOrfs}}.
+#' 
+#' @examples
+#' # Using a genome file in this package
+#' genome.file <- file.path(path.package("microseq"),"extdata","small.fna")
+#' 
+#' # Reading genome and finding orfs
+#' genome <- readFasta(genome.file)
+#' orf.tbl <- findOrfs(genome)
+#' 
+#' # Compute signatures
+#' signature.full <- orfSignature(orf.tbl)
+#' signature.reduced <- orfSignature(orf.tbl, full = FALSE)
+#' 
+#' @importFrom dplyr if_else
+#' @importFrom stringr str_c
+#' 
+#' @export orfSignature
+#' 
+orfSignature <- function(orf.table, full = TRUE){
+  if(full){
+    signature <- str_c(orf.table$Seqid,
+                       orf.table$Start,
+                       orf.table$End,
+                       orf.table$Strand,
+                       sep = ";")
+  } else {
+    signature <- if_else(orf.table$Strand == "+",
+                         str_c(orf.table$Seqid,
+                               orf.table$End,
+                               orf.table$Strand,
+                               sep = "_"),
+                         str_c(orf.table$Seqid,
+                               orf.table$Start,
+                               orf.table$Strand,
+                               sep = ";"))
+  }
+  return(signature)
 }
